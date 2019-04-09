23fdfb8dd3b87cb4206d45709e88e9cec0110048
Pasha Afanasev <afanasev_p@ukr.net>
2019-04-09 16:23:23 +0300

    db device issue

--- db/migrate/20190303204411_create_devices.rb
+++ db/migrate/20190303204411_create_devices.rb
@@ -1,8 +1,6 @@
 class CreateDevices < ActiveRecord::Migration[5.2]
   def change
     create_table :devices do |t|
-      t.references :category, foreign_key:true
-      t.references :brand, foreign_key:true
       t.references :model, foreign_key:true
       t.string :serial_num, index: true
       t.string :imei, index: true
